736b9a94bfd70dd2e8699bf0c04cec9f9661e3dc
Juliana <omoreiraju@gmail.com>
2022-07-11 21:54:40 -0300

    Melhorias na lista de funcionários

--- Rh/admin.py
+++ Rh/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from Rh.models import Funcionario, Departamento
 
+
+class FuncionarioAdmin(admin.modeladmin):
+    list_display = ['nome','departamento', 'cargo']
+    list_filter = ['Departamento', 'Cargo']
+
+
 # Register your models here.
-admin.site.register(Funcionario)
+admin.site.register(Funcionario, FuncionarioAdmin)
 admin.site.register(Departamento)
